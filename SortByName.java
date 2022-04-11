7da6081286d9491d412b554caf5ad1ac90592798
Glicore <74731829+Glicore@users.noreply.github.com>
2022-04-11 12:15:14 -0500

    Delete SortByName.java

--- SortByName.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class SortByName implements Comparator<MiniMart_Records>
-{
-    public int compare(MiniMart_Records p1, MiniMart_Records p2)
-    {
-        return CharSequence.compare(p1.getName(), p2.getName());
-    }
-}
